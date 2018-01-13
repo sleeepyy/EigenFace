b880783e0986f63813cad26a309e6914b3c6ba81
sleepy <yanghan9826@gmail.com>
2018-01-13 23:08:14 +0800

    commit

---
+++ facelib.h
@@ -0,0 +1,87 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+
+// Bad Implementation Because of my Laziness.
+// Plz Don't Use Namespace in *.h as me.
+using namespace std;
+using namespace cv;
+
+const int WIDTH = 73;
+const int HEIGHT = 89;
+
+
+class FaceEntry {
+public:
+	Mat origin_pic;
+	Mat gray_pic;
+	Mat transformed_pic;
+	int x1, y1, x2, y2;
+	Mat trans_mat;
+	Mat_<double> equalized_mat;
+	Mat_<double> vect;
+	void load(string& path, string ext) {
+		load_eye_pos(path);
+		origin_pic = imread(path + ext);
+		gray_pic = imread(path + ext, IMREAD_GRAYSCALE);
+		transform();
+	}
+	void load(string& path) {
+		load_eye_pos(path.substr(0, path.length()-4));
+		origin_pic = imread(path);
+		gray_pic = imread(path, IMREAD_GRAYSCALE);
+		transform();
+	}
+
+	void load_eye_pos(string& path) {
+		ifstream file(path + ".txt", ifstream::in);
+		file >> x1 >> y1 >> x2 >> y2;
+	}
+
+	void transform() {
+		Point center((x1 + x2) / 2, (y1 + y2) / 2);
+		double angle = atan((double)(y2 - y1) / (double)(x2 - x1)) * 180.0 / CV_PI;
+		trans_mat = getRotationMatrix2D(center, angle, 1.0);
+		trans_mat.at<double>(0, 2) += 37 - center.x;
+		trans_mat.at<double>(1, 2) += 30 - center.y;
+		warpAffine(gray_pic, transformed_pic, trans_mat, gray_pic.size()*4/5);
+		equalizeHist(transformed_pic, transformed_pic);
+		transformed_pic.copyTo(equalized_mat);
+		vect = equalized_mat.reshape(1, 1).t();
+	}
+
+};
+
+class FaceLib {
+public:
+	int num_of_faces = 202;
+	int num_of_persons = 40;
+	int faces_per_person = 5;
+	vector<FaceEntry*> faces;
+	vector<Mat_<double>> _samples;
+	Mat_<double> samples;
+
+	void load(string& path) {
+		for (int i = 1; i <= num_of_persons; i++)
+		{
+			for (int j = 1; j <= faces_per_person; ++j) {
+				string entry_path = path + "/s" + to_string(i) + "/" + to_string(j);
+				FaceEntry* face = new FaceEntry();
+				face->load(entry_path, ".pgm");
+				faces.push_back(face);
+				_samples.push_back(face->vect);
+			}
+		}
+		for (int j = 1; j <= 2; ++j) {
+			string entry_path = path + "/s" + to_string(41) + "/" + to_string(j);
+			FaceEntry* face = new FaceEntry();
+			face->load(entry_path, ".png");
+			faces.push_back(face);
+			_samples.push_back(face->vect);
+		}
+		hconcat(_samples, samples);
+	}
+};
---
+++ test.cpp
@@ -0,0 +1,77 @@
+#include "facelib.h"
+
+int main(int argc, char** argv) {
+	FaceLib facelib;
+	facelib.load(string("att_faces"));
+
+	char* model_name = "eigen.model";
+	char* file_name = "att_faces/s41/3.png";
+	if (argc >= 3) {
+		model_name = argv[2];
+		file_name = argv[1];
+	}
+	FileStorage model(model_name, FileStorage::READ);
+	Mat e_vector_mat, e_value_mat;
+	model["e_vector_mat"] >> e_vector_mat;
+	model["e_value_mat"] >> e_value_mat;
+	Mat distance;
+	Mat samples;
+	FaceEntry face;
+	facelib.samples.copyTo(samples);
+	distance = e_vector_mat * samples;
+	for (int _i = 1; _i <= 41; ++_i) {
+		for (int _j = 1; _j <= 10; _j++)
+		{
+			if (_i == 41) {
+				face.load(string("att_faces/s") + to_string(_i) + "/" + to_string(_j), ".png");
+			}
+			else {
+				face.load(string("att_faces/s") + to_string(_i) + "/" + to_string(_j), ".pgm");
+			}
+			Mat face_vect = e_vector_mat * face.vect;
+			double min_d = norm(face_vect, distance.col(0), NORM_L2);
+			double temp_d = 0;
+			int min_i = 0;
+
+			for (int i = 1; i < distance.cols; ++i) {
+				temp_d = norm(face_vect, distance.col(i), NORM_L2);
+				if (temp_d <= min_d) {
+					min_d = temp_d;
+					min_i = i;
+				}
+			}
+			cout << (min_i/5)+1 << "/" << (min_i % 5)+1 << " ";
+			if (_i == 41 && _j == 3)break;
+		}
+		cout << endl;
+	}
+
+	face.load(string(file_name));
+	
+	Mat face_vect = e_vector_mat * face.vect;
+	double min_d = norm(face_vect, distance.col(0), NORM_L2);
+	double temp_d = 0;
+	int min_i = 0;
+
+	for (int i = 1; i < distance.cols; ++i) {
+		temp_d = norm(face_vect, distance.col(i), NORM_L2);
+		if (temp_d <= min_d) {
+			min_d = temp_d;
+			min_i = i;
+		}
+	}
+	cout << (min_i / 5) + 1 << "/" << (min_i % 5) + 1 << " " << endl;
+	Mat origin_mat = face.origin_pic;
+	Mat similar_mat = facelib.faces.at(min_i)->origin_pic;
+	string text = "s" + to_string(min_i / 5 + 1) + " No." + to_string(min_i % 5 + 1);
+	cout << text << endl;
+	putText(origin_mat, text, Point(10, 20), FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 255), 2, 8);
+	imshow("FaceResult", origin_mat);
+	imshow("Similar Pic", similar_mat);
+	waitKey(0);
+	destroyAllWindows();
+	return 0;
+
+
+
+}
